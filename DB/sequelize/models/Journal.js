89364aa8610c5e7a03c3ac3236bc216def429f96
belljun3395 <195850@jnu.ac.kr>
2022-07-19 06:44:29 +0900

    Feat  : JournalService 구현

--- DB/sequelize/models/Journal.js
+++ DB/sequelize/models/Journal.js
@@ -3,27 +3,17 @@ const { DataTypes, Sequelize } = require('sequelize');
 module.exports = class Journal extends Sequelize.Model {
   static init(sequelize) {
     return super.init({
-      journalCategory: {
+      journalDate: {
         type: DataTypes.STRING(100),
         allowNull: true,
         unique: false,
       },
-      journalTitle: {
-        type: DataTypes.STRING(100),
-        allowNull: true,
-        unique: false,
-      },
-      journalAuthor: {
-        type: DataTypes.STRING(100),
-        allowNull: true,
-        unique: false,
-      },
-      journalCount: {
+      journalHours: {
         type: DataTypes.INTEGER(100),
         allowNull: true,
         unique: false,
       },
-      journalContent: {
+      journalEtc: {
         type: DataTypes.STRING(300),
         allowNull: true,
         unique: false,
